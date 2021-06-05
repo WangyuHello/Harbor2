ea2ed8583fc83de9e293a7a59e81eae0444a148d
王渝 <wwangyuu@outlook.com>
2021-06-05 17:33:37 +0800

    优化Python引擎

--- Harbor.Python/PythonHelper.cs
+++ Harbor.Python/PythonHelper.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using Python.Runtime;
 
 namespace Harbor.Python
@@ -30,6 +27,28 @@ namespace Harbor.Python
             }
         }
 
+        private static void SetForWindows()
+        {
+            var pathToVirtualEnv = @"C:\Python\Python38";
+            Runtime.PythonDLL = $"{pathToVirtualEnv}\\python38.dll";
+            PythonEngine.PythonHome = pathToVirtualEnv;
+            Environment.SetEnvironmentVariable("PATH", $"{pathToVirtualEnv};E:\\Tools\\iverilog\\bin", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("PYTHONHOME", pathToVirtualEnv, EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("PYTHONPATH", $"{pathToVirtualEnv}\\Lib\\site-packages;{pathToVirtualEnv}\\Lib", EnvironmentVariableTarget.Process);
+            PythonEngine.PythonPath = PythonEngine.PythonPath + ";" + Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Process);
+        }
+
+        public static void SetForLinuxServer()
+        {
+            var pathToVirtualEnv = "/export/yfxie02/bin/python";
+            Runtime.PythonDLL = $"{pathToVirtualEnv}/lib/libpython3.8.so.1.0";
+            PythonEngine.PythonHome = pathToVirtualEnv;
+            Environment.SetEnvironmentVariable("PATH", $"{pathToVirtualEnv}/bin:" + Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process), EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("PYTHONHOME", pathToVirtualEnv, EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("PYTHONPATH", $"{pathToVirtualEnv}/lib/python3.8/site-packages:{pathToVirtualEnv}/lib", EnvironmentVariableTarget.Process);
+            PythonEngine.PythonPath = PythonEngine.PythonPath + ":" + Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Process);
+        }
+
         public static void SetEnvironment(string workingDirectory, Action code)
         {
             var curDir = Environment.CurrentDirectory;
@@ -39,23 +58,18 @@ namespace Harbor.Python
             {
                 PythonEngine.Shutdown();
             }
-#if DEBUG
-            var pathToVirtualEnv = @"C:\Python\Python38";
-            Runtime.PythonDLL = $"{pathToVirtualEnv}\\python38.dll";
-            PythonEngine.PythonHome = pathToVirtualEnv;
-            Environment.SetEnvironmentVariable("PATH", $"{pathToVirtualEnv};E:\\Tools\\iverilog\\bin", EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("PYTHONHOME", pathToVirtualEnv, EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("PYTHONPATH", $"{pathToVirtualEnv}\\Lib\\site-packages;{pathToVirtualEnv}\\Lib", EnvironmentVariableTarget.Process);
-            PythonEngine.PythonPath = PythonEngine.PythonPath + ";" + Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Process);
-#else
-            var pathToVirtualEnv = "/export/yfxie02/bin/python";
-            Runtime.PythonDLL = $"{pathToVirtualEnv}/lib/libpython3.8.so.1.0";
-            PythonEngine.PythonHome = pathToVirtualEnv;
-            Environment.SetEnvironmentVariable("PATH", $"{pathToVirtualEnv}/bin:" + Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process), EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("PYTHONHOME", pathToVirtualEnv, EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("PYTHONPATH", $"{pathToVirtualEnv}/lib/python3.8/site-packages:{pathToVirtualEnv}/lib", EnvironmentVariableTarget.Process);
-            PythonEngine.PythonPath = PythonEngine.PythonPath + ":" + Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Process);
-#endif
+            else
+            {   //初次运行
+                if (Environment.OSVersion.Platform == PlatformID.Unix)
+                {
+                    SetForLinuxServer();
+                }
+                else
+                {
+                    SetForWindows();
+                }
+            }
+
             using (Py.GIL())
             {
                 code?.Invoke();
